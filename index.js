ab4cde970a23868f0476cd8190ed2aa57ed920d6
karina <karinav19817@gmail.com>
2022-03-05 22:05:16 +0300

    feat: change file function

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const contrastElement = document.getElementById("contrast");
 const hueRotateElement = document.getElementById("hue-rotate");
 const sepiaElement = document.getElementById("sepia");
 
-const uploadButtonElement = document.getElementById("upload-button");
+const fileInputElement = document.getElementById("file-input");
+const fileNameElement = document.getElementById("file-name");
 const chosenImageElement = document.getElementById("chosen-image");
 const defaultImageElement = document.getElementById("default-image");
 const wrapperImageElement = document.querySelector(".wrapper-image");
 const wrapperInputElement = document.querySelector(".wrapper-input");
 const submitButtonElement = document.getElementById("submit-button");
+const imageCompareSliderElement = document.getElementById("img-comp-slider")
 
 const imageContainerElement = document.querySelector(".image-container");
 const sliders = document.querySelectorAll(".filter input[type='range']");
+const imgCompOverlayElement = document.getElementById("img-comp-overlay");
+
 
 
 submitButtonElement.addEventListener('click',() => {
@@ -19,15 +23,14 @@ submitButtonElement.addEventListener('click',() => {
   wrapperInputElement.style.display = "none";
 })
 
-const inputElement = wrapperInputElement.getElementsByTagName('input')
-Array.prototype.forEach.call(inputElement, function(item) {
-  item.addEventListener("change", changeFileName)
+
+function attachEventListenerToFileInput() {
+  fileInputElement.addEventListener("change", changeFileName)
   function changeFileName(){
-    let fileName = '';
-    fileName = this.value.split("\\").slice(-1)[0];
-    this.parentNode.nextElementSibling.innerHTML = fileName;
+    const file = fileInputElement.files[0];
+    fileNameElement.innerHTML = file.name;
   }
-});
+}
 
 function resetFilter(){
   blurElement.value = "0";
@@ -37,10 +40,10 @@ function resetFilter(){
   addFilter();
 } 
 
-uploadButtonElement.onchange = () => {
+fileInputElement.onchange = () => {
   resetFilter();
   let reader = new FileReader();
-  reader.readAsDataURL(uploadButtonElement.files[0])
+  reader.readAsDataURL(fileInputElement.files[0])
   reader.addEventListener("load", function(){
       chosenImageElement.setAttribute("src", reader.result)
       defaultImageElement.setAttribute("src", reader.result)
@@ -57,65 +60,9 @@ function addFilter(){
 
 
 
-function initComparisons() {
-    /*найти все элементы с классом "overlay":*/
-    const imgCompOverlayElement = document.getElementsByClassName("img-comp-overlay");
-    for (let i = 0; i < imgCompOverlayElement.length; i++) {
-      compareImages(imgCompOverlayElement[i]);
-    }
-    function compareImages(img) {
-      var slider, img, clicked = 0;
-      /*получить ширину и высоту элемента img*/
-      let imgWidth = img.offsetWidth;
-      let imgHeight = img.offsetHeight;
-      /*установите ширину элемента img на 50%:*/
-      img.style.width = (imgWidth / 2) + "px";
-      /*создать слайдер:*/
-      slider = document.createElement("DIV");
-      slider.setAttribute("class", "img-comp-slider");
-      /*вставить ползунок*/
-      img.parentElement.insertBefore(slider, img);
-      /*расположите ползунок посередине:*/
-      slider.style.top = (imgHeight / 2) - (slider.offsetHeight / 2) + "px";
-      slider.style.left = (imgWidth / 2) - (slider.offsetWidth / 2) + "px";
-      /*выполнение функции при нажатии кнопки мыши:*/
-      slider.addEventListener("mousedown", slideReady);
-      /*и еще одна функция при отпускании кнопки мыши:*/
-      window.addEventListener("mouseup", slideFinish);
-      /*или прикоснулся (для сенсорных экранов:*/
-      slider.addEventListener("touchstart", slideReady);
-      /*и выпущенный (для сенсорных экранов:*/
-      window.addEventListener("touchstop", slideFinish);
-
-
-      function slideReady(e) {
-        /*предотвратите любые другие действия, которые могут произойти при перемещении по изображению:*/
-        e.preventDefault();
-        /*теперь ползунок нажат и готов к перемещению:*/
-        clicked = 1;
-        /*выполнение функции при перемещении ползунка:*/
-        window.addEventListener("mousemove", slideMove);
-        window.addEventListener("touchmove", slideMove);
-      }
-
-      function slideFinish() {
-        /*ползунок больше не нажимается:*/
-        clicked = 0;
-      }
-
-      function slideMove(e) {
-        let pos;
-        /*если ползунок больше не нажат, выйдите из этой функции:*/
-        if (clicked == 0) return false;
-        /*получить х положения курсора :*/
-        pos = getCursorPos(e)
-        /*не допускайте расположения ползунка вне изображения:*/
-        if (pos < 0) pos = 0;
-        if (pos > imgWidth) pos = imgWidth;
-        /*выполните функцию, которая изменит размер наложенного изображения в соответствии с курсором:*/
-        slide(pos);
-      }
-
+function compareImage(img) {
+  function slideReady(e) {
+    function slideMove(e) {
       function getCursorPos(e) {
         let a, x = 0;
         e = e || window.event;
@@ -127,14 +74,55 @@ function initComparisons() {
         x = x - window.pageXOffset;
         return x;
       }
-      
+  
       function slide(x) {
         /*изменение размера изображения:*/
         img.style.width = x + "px";
         /*расположите ползунок:*/
-        slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+        imageCompareSliderElement.style.left = img.offsetWidth - (imageCompareSliderElement.offsetWidth / 2) + "px";
       }
+  
+      let pos;
+      /*если ползунок больше не нажат, выйдите из этой функции:*/
+      if (clicked == 0) return false;
+      /*получить х положения курсора :*/
+      pos = getCursorPos(e)
+      /*не допускайте расположения ползунка вне изображения:*/
+      if (pos < 0) pos = 0;
+      if (pos > imgWidth) pos = imgWidth;
+      /*выполните функцию, которая изменит размер наложенного изображения в соответствии с курсором:*/
+      slide(pos);
     }
+
+    /*предотвратите любые другие действия, которые могут произойти при перемещении по изображению:*/
+    e.preventDefault();
+    /*теперь ползунок нажат и готов к перемещению:*/
+    clicked = 1;
+    /*выполнение функции при перемещении ползунка:*/
+    window.addEventListener("mousemove", slideMove);
   }
 
-  initComparisons()
+  function slideFinish() {
+    /*ползунок больше не нажимается:*/
+    clicked = 0;
+  }
+
+
+  var clicked = 0;
+  /*получить ширину и высоту элемента img*/
+  let imgWidth = img.offsetWidth;
+  let imgHeight = img.offsetHeight;
+  /*установите ширину элемента img на 50%:*/
+  img.style.width = (imgWidth / 2) + "px";
+  /*расположите ползунок посередине:*/
+  imageCompareSliderElement.style.top = (imgHeight / 2) - (imageCompareSliderElement.offsetHeight / 2) + "px";
+  imageCompareSliderElement.style.left = (imgWidth / 2) - (imageCompareSliderElement.offsetWidth / 2) + "px";
+  /*выполнение функции при нажатии кнопки мыши:*/
+  imageCompareSliderElement.addEventListener("mousedown", slideReady);
+  /*и еще одна функция при отпускании кнопки мыши:*/
+  window.addEventListener("mouseup", slideFinish);
+}
+
+attachEventListenerToFileInput()
+
+compareImage(imgCompOverlayElement);
